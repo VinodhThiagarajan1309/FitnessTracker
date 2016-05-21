1fd85c0cda1d5c45ae0b734ecabdc4cf5b6608c2
Vinodh Thiagarajan <vinodhthiagarajan@Vinodhs-MacBook-Pro.local>
2016-05-21 03:45:43 -0500

    Committing completed Project Setup

---
+++ FitnessTracker/src/main/java/info/javagrasp/controller/GoalController.java
@@ -0,0 +1,40 @@
+package info.javagrasp.controller;
+
+import javax.validation.Valid;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.SessionAttributes;
+
+import info.javagrasp.model.Goal;
+
+@Controller
+@SessionAttributes("goal")
+public class GoalController {
+	
+	@RequestMapping(value="addGoal" , method = RequestMethod.GET)
+	public String addGoal(Model model){
+		Goal goal = new Goal();
+		goal.setMinutes(10);
+		model.addAttribute("goal", goal);
+		return "addGoal";
+	}
+	
+	@RequestMapping(value="addGoal" , method = RequestMethod.POST)
+	public String updateGoal(@Valid @ModelAttribute("goal") Goal goal , BindingResult result){
+		
+		System.out.println("Result has error "+ result.hasErrors());
+	
+		System.out.println("Minutes Updated : " +goal.getMinutes());
+		if(result.hasErrors()){
+			return "addGoal";
+		}
+		
+		return "redirect:addMinutes.html";
+	}
+
+}
